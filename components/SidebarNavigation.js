939c509ae2716b59be048975585424b73a7ef023
Ivan H <ivanhrybe@gmail.com>
2023-11-17 01:37:41 +0100

    feat(sidebar): implement collapsible functionality and styling
    Added the collapsible feature to the sidebar, allowing users to toggle the sidebar's visibility. Implemented responsive styling for the logo and menu items to adjust smoothly during collapse and expansion.

--- components/SidebarNavigation.js
+++ components/SidebarNavigation.js
@@ -1,9 +1,51 @@
+// components/Navigation.js
+import React, { useState } from 'react';
+import Image from 'next/image';
+import styles from '../styles/SidebarNavigation.module.css';
 
 
-export default function SidebarNavigation () {
-    return(
-        <>
-        </>
-    )
+const menuItems = [
+    { icon: 'person_4', title: 'Cabinet personnel ', link: '/home', class: 'material-symbols-outlined' },
+    { icon: 'home_app_logo', title: 'Accueil', link: '/home', class: 'material-symbols-outlined' },
+    { icon: 'chat_bubble', title: 'Messagerie', link: '/home', class: 'material-symbols-outlined' },
+    { icon: 'empty_dashboard', title: 'Tableau de bord ', link: '/dashboard', class: 'material-symbols-outlined' },
+    { icon: 'style', title: 'Cartes de révision', link: '/home', class: 'material-symbols-outlined' },
+    { icon: 'history_edu', title: 'Cours ', link: '/home', class: 'material-symbols-outlined' },
+    { icon: 'communities', title: 'Communauté ', link: '/home', class: 'material-symbols-outlined' },
+    { icon: 'settings', title: 'Paramètres  ', link: '/home', class: 'material-symbols-outlined' },
+    { icon: 'logout', title: 'Déconnexion   ', link: '/home', class: 'material-symbols-outlined' },
+];
+const SidebarNavigation = ({ onToggle }) => {
+    const [isCollapsed, setIsCollapsed] = useState(false);
 
-}
+    const toggleCollapse = () => {
+        setIsCollapsed(!isCollapsed);
+        onToggle();
+    };
+
+    return (
+        <div className={`${styles.nav} ${isCollapsed ? styles.collapsed : ''}`}>
+            <div className={`${styles.logo_container} ${isCollapsed ? styles.collapsedLogo : ''}`}>
+                <Image
+                    src="/Logo/Logo.svg"
+                    alt="Logo de Revision Zen"
+                    layout="fill"  // Utilisez "fill" pour un redimensionnement responsive
+                    objectFit="contain"  // Gardez les proportions du logo
+                />
+            </div>
+            <div className={styles.menuItems}>
+                {menuItems.map((item, index) => (
+                    <a key={index} href={item.link} className={styles.menuItem}>
+                        <span className={`${styles.icon} ${item.class}`}>{item.icon}</span>
+                        <span className={`${styles.title} ${isCollapsed ? styles.hidden :''}`}>{item.title}</span>
+                    </a>
+                ))}
+            </div>
+            <span onClick={toggleCollapse} className={`${styles.toggleIcon} material-icons ${isCollapsed ? styles.rotated : ''}`}>
+                arrow_back
+            </span>
+        </div>
+    );
+};
+
+export default SidebarNavigation;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,23 @@
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+// pages/index.js
+import React, { useState } from 'react';
+import SidebarNavigation from '../components/SidebarNavigation';
+import styles from '../styles/Home.module.css';
+import Head from "next/head";
 
 export default function Home() {
-  return (
-    <>
+    const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(false);
 
-    </>
-  )
+    return (
+        <div className={styles.pageContainer}>
+            <Head>
+                <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
+                <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+            </Head>
+            <SidebarNavigation onToggle={() => setIsSidebarCollapsed(prev => !prev)} />
+            <main className={`${styles.mainContent} ${!isSidebarCollapsed ? styles.expandedMain : ''}`}>
+                <h1>Hello World</h1>
+                {/* Autres contenus */}
+            </main>
+        </div>
+    )
 }
